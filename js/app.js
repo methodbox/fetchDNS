006d254b85c42fe81f11060c9e612a5bd1cbcb32
Chris Shaffer <chris@methodbox.io>
2017-07-01 11:32:46 -0500

    complete separation of methods to pure functions, added specific DNS record type query for host names that deny ANY type request

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //  create our view in Vue.js
 const dnsView = new Vue({
   el: '#dns',
-  data: {
+  data: { //  define our simple schema structure
     message: '',
     domain: '',
     soaRecs: [],
@@ -10,79 +10,86 @@ const dnsView = new Vue({
     mxRecs: [],
     txtRecs: [],
     searchComplete: false,
-    versionNum: '1.3.2',
-    versionDate: '6.23.2017'
+    versionNum: '1.4',
+    versionDate: '7.1.2017'
   },
   methods: {
-
-    getSearchField: function() {
-      let getSearchData = document.getElementById('dns-search-input') //  get our search field value
+    getSearchField: function () { // grab and parse the value in search field
+      let getSearchData = document.getElementById('dns-search-input')
       let cleanSearchData = getSearchData.value.split('/')
       let searchFilter = cleanSearchData.filter((domain) => {
         if (domain !== 'https:' && domain !== 'http:' && domain !== '') {
-          dnsView.getDNS(domain)
+          dnsView.fetchDNS(domain) // pass just the parsed domain to the dns fetch function
         }
       })
     },
-    getDNS: function(domain) { //  fetch method - get our data on-click
-      dnsView.soaRecs = [] // set all record values to empty to avoid duplicates
+    clearField: function () { // clear our search field on click
+      document.getElementById('dns-search-input').value = ''
+    },
+    clearRecords: function () { // set all record values to empty to avoid duplicates
+      dnsView.searchComplete = false
+      dnsView.soaRecs = []
       dnsView.aRecs = []
       dnsView.nsRecs = []
       dnsView.mxRecs = []
       dnsView.txtRecs = []
-    getDNS: function(domain) { //  fetch method - get our data on-click
+    },
+    fetchDNS: function (domain) { //  fetch method - get our data on-click
       dnsView.clearRecords()
-      let allDNS = 'https://dns.google.com/resolve?name=' + domain + '&type=ANY' //  define our query
-      fetch(allDNS) //  get all the DNS
+      let anyDNS = 'https://dns.google.com/resolve?name=' + domain + '&type=ANY' //  define our query
+      fetch(anyDNS) //  get all the DNS
         .then((resp) => resp.json())
         .then((data) => {
           let dns = data.Answer
-          const dnsFilter = dns.filter((answer) => {
-            switch(answer.type) {
-              case 1: //  all switch cases = dns record numerical types
-                dnsView.aRecs.push({aRec: {value: answer.data, TTL: answer.TTL}})
-                break
-              case 2:
-                dnsView.nsRecs.push({nsRec: {value: answer.data, TTL: answer.TTL}})
-                break
-              case 6:
-                dnsView.soaRecs.push({soaRec: {value: answer.data, TTL: answer.TTL}})
-                break
-              case 15:
-                dnsView.mxRecs.push({mxRec: {value: answer.data.substr(2) + " " + "Priority: " + answer.data.substr(0, 1),TTL: answer.TTL}})
-                break
-              case 16:
-                dnsView.txtRecs.push({txtRec: {value: answer.data, TTL: answer.TTL}})
-                break
-            }
-          })
-          dnsView.searchComplete = true
+          if (dns[0].type === 13) { //  pass to specific query if record type 'ANY' is denied (DNSSec)
+            this.getSpecificDNS(domain)
+          } else {
+            this.dnsFilter(dns, domain) //  pass to filter to parse record types
+            dnsView.searchComplete = true
+          }
         })
       .catch(function(error) {
         console.log(error) //  log any errors to the console
       })
     },
-    clearField: function () {
-      document.getElementById('dns-search-input').value = ''
-    getSearchField: function() {
-      let getSearchData = document.getElementById('dns-search-input') //  get our search field value
-      let cleanSearchData = getSearchData.value.split('/')
-      let searchFilter = cleanSearchData.filter((domain) => {
-        if (domain !== 'https:' && domain !== 'http:' && domain !== '') {
-          dnsView.getDNS(domain)
+    getSpecificDNS: function (domain) {
+      let dnsQuery = 'https://dns.google.com/resolve?name=' +  domain + '&type='
+      let aQuery = dnsQuery + '1' //  query by specific record type
+      let nsQuery = dnsQuery + '2'
+      let soaQuery = dnsQuery + '6'
+      let mxQuery = dnsQuery + '15'
+      let txtQuery = dnsQuery + '16'
+      let dnsQueryArray = [aQuery, nsQuery, soaQuery, mxQuery, txtQuery] // array of specific record types
+      dnsQueryArray.map((dns) => { // map array for multiple queries before filtering
+        fetch(dns)
+          .then((res) => res.json())
+          .then((data) => {
+            let allDNS = data.Answer
+            this.dnsFilter(allDNS) // pass each array value to record type filter
+          })
+      })
+      dnsView.searchComplete = true
+    },
+    dnsFilter: function (answer) {
+      answer.filter((answer) => { //  filter the record types by value, return the value to our data() along w/TTL value
+        switch(answer.type) {
+          case 1:
+            dnsView.aRecs.push({aRec: {value: answer.data, TTL: answer.TTL}})
+            break
+          case 2:
+            dnsView.nsRecs.push({nsRec: {value: answer.data, TTL: answer.TTL}})
+            break
+          case 6:
+            dnsView.soaRecs.push({soaRec: {value: answer.data, TTL: answer.TTL}})
+            break
+          case 15:
+            dnsView.mxRecs.push({mxRec: {value: answer.data.substr(2) + " " + "Priority: " + answer.data.substr(0, 1),TTL: answer.TTL}})
+            break
+          case 16:
+            dnsView.txtRecs.push({txtRec: {value: answer.data, TTL: answer.TTL}})
+            break
         }
       })
-    },
-    clearField: function () {
-      document.getElementById('dns-search-input').value = ''
-    },
-    clearRecords: function () {
-      dnsView.searchComplete = false
-      dnsView.soaRecs = [] // set all record values to empty to avoid duplicates
-      dnsView.aRecs = []
-      dnsView.nsRecs = []
-      dnsView.mxRecs = []
-      dnsView.txtRecs = []
     }
   }
 })
