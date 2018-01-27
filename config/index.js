583aa8e1cabeb60746be6388a8f1ab615cd27401
Chris Shaffer <chris@methodbox.io>
2018-01-26 19:12:10 -0600

    updating public docs for github pages

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: './',
+    assetsSubDirectory: 'https://methodbox.github.io/fetchDNS/',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
