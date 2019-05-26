bffb2b3f988e4ccae974c38dd19f142349a0d8a9
Chris Shaffer <chris@methodbox.io>
2019-05-25 22:17:43 -0500

    added stronger typing. added auto-remove li fields

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import fetch, { Response } from 'node-fetch';
 import { updateTextFields } from 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 import './Dns.css';
+import validator from 'validator';
 
 export type Props = {};
 export type State = {
-  typeA1: any;
-  typeNS2: any;
-  typeCname5: any;
-  typeSoa6: any;
-  typeMx15: any;
-  typeTxt16: any;
+  typeA1: Array<object>;
+  typeNS2: Array<object>;
+  typeCname5: Array<object>;
+  typeSoa6: Array<object>;
+  typeMx15: Array<object>;
+  typeTxt16: Array<object>;
   record: Array<object> | any;
   domainInput: string;
 };
 
+export interface DNS extends Response {
+  Answer?: Array<object>;
+  //  fixing a TS bug until it's actually fixed
+  forEach(callbackfn: (value: any, index: number, array: any[]) => void, thisArg?: any): void;
+}
+
 export type Record = {
+  Answer?: Array<object>;
   type?: number;
   data?: string;
   TTL?: string;
@@ -28,6 +36,7 @@ export interface JSON {
   data?: string;
   TTL?: string;
 }
+
 export default class FetchDNS extends Component<Props, State> {
   state: State = {
     typeA1: [],
@@ -46,66 +55,71 @@ export default class FetchDNS extends Component<Props, State> {
 
   _onChangeDomain = (event: string) => {
     this.setState({ domainInput: event });
-    console.log(this.state.domainInput);
-  };
-  _onGetDns = (domain: string | null) => {
-    let dnsQuery = `https://dns.google.com/resolve?name=${domain}&type=`;
-    /**
-     * @dns
-     * Record types: | 1 = A record | 2 = NS | 6 = SOA | 15 = MX | 16 = TXT
-     */
-    let dnsQueryArray: Array<string> = [
-      `${dnsQuery}1`,
-      `${dnsQuery}2`,
-      `${dnsQuery}6`,
-      `${dnsQuery}15`,
-      `${dnsQuery}16`,
-    ];
+    console.log(this.state.domainInput.length)
 
-    dnsQueryArray.forEach(
-      (dns: string): void => {
-        fetch(dns)
-          .then(
-            (response: Response): Promise<JSON> => {
-              return response.json();
-            }
-          )
-          .then((json: JSON) => {
-            let answer = json.Answer;
-            console.log(answer);
-            if (answer !== undefined) {
-              answer.forEach(
-                (record: Record): void => {
-                  switch (record.type) {
+    if (this.state.domainInput.length <= 1) {
+      this.setState({
+        typeA1: [], typeCname5: [], typeMx15: [], typeNS2: [], typeSoa6: [], typeTxt16: []
+      })
+    }
+  };
+  _onGetDns = (domain: string) => {
+    if (validator.isFQDN(domain)) {
+      let dnsQuery = `https://dns.google.com/resolve?name=${domain}&type=`;
+      /**
+       * @dns
+       * Record types: | 1 = A record | 2 = NS | 6 = SOA | 15 = MX | 16 = TXT
+       */
+      let dnsQueryArray: Array<string> = [
+        `${dnsQuery}1`,
+        `${dnsQuery}2`,
+        `${dnsQuery}6`,
+        `${dnsQuery}15`,
+        `${dnsQuery}16`,
+      ];
+
+      dnsQueryArray.forEach(
+        (dns: string): void => {
+          fetch(dns)
+            .then(
+              (response: Response): Promise<any> => {
+                return response.json();
+              }
+            )
+            .then((json: Record): void => {
+              if (json.Answer !== undefined) {
+                json.Answer.forEach((data: Record): void => {
+                  switch (data.type) {
                     case 1:
-                      this.setState({ typeA1: json.Answer });
+                      this.setState({ typeA1: (json.Answer) ? json.Answer : [{ default: 'default' }] });
                       break;
                     case 2:
-                      this.setState({ typeNS2: json.Answer });
+                      this.setState({ typeNS2: (json.Answer) ? json.Answer : [{ default: 'default' }] });
                       break;
                     case 5:
-                      this.setState({ typeCname5: json.Answer });
+                      this.setState({ typeCname5: (json.Answer) ? json.Answer : [{ default: 'default' }] });
                       break;
                     case 6:
-                      this.setState({ typeSoa6: json.Answer });
+                      this.setState({ typeSoa6: (json.Answer) ? json.Answer : [{ default: 'default' }] });
                       break;
                     case 15:
-                      this.setState({ typeMx15: json.Answer });
+                      this.setState({ typeMx15: (json.Answer) ? json.Answer : [{ default: 'default' }] });
                       break;
                     case 16:
-                      this.setState({ typeTxt16: json.Answer });
-                      console.log('STATE: ', this.state.typeTxt16);
+                      this.setState({ typeTxt16: (json.Answer) ? json.Answer : [{ default: 'default' }] });
                       break;
                   }
                 }
-              );
-            } else {
-              console.log('No record found.');
-            }
-          })
-          .catch(err => console.log(err));
-      }
-    );
+
+                );
+              } else {
+                console.log('No record found.');
+              }
+            })
+            .catch(err => console.log(err));
+        }
+      );
+    }
   };
 
   render() {
