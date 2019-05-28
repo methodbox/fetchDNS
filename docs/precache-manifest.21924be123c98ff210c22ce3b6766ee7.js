c51e3e105b3484ec55111bb6d2114bdc7af562b1
Chris Shaffer <chris@methodbox.io>
2019-05-27 21:59:51 -0500

    prep for github pages

---
+++ docs/precache-manifest.21924be123c98ff210c22ce3b6766ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de7b9022ac17e5cbc3ebaa0dcf33a4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49ec896baf6bd58e371b",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "f52e1ad67497af48d6b6",
+    "url": "/static/css/main.a6785677.chunk.css"
+  },
+  {
+    "revision": "49ec896baf6bd58e371b",
+    "url": "/static/js/2.e7b02c8b.chunk.js"
+  },
+  {
+    "revision": "f52e1ad67497af48d6b6",
+    "url": "/static/js/main.4e76b051.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
