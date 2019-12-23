5a26536fc5d6ac16b682cffb65d5c16f8ac0b5e3
Chris Shaffer <chris@methodbox.io>
2019-12-22 19:58:34 -0600

    Revert "commit because create-react-app sucks"
    This reverts commit f5dfeea6f282faf13e7a406f4fc825ce3d767908.

---
+++ docs/precache-manifest.7ef4af3e3c75f3062d4f6c01b82ed0fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edce675ced8485ff14127153dc485e64",
+    "url": "/fetchDNS/index.html"
+  },
+  {
+    "revision": "49ec896baf6bd58e371b",
+    "url": "/fetchDNS/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "4afcba7b00242bd16b89",
+    "url": "/fetchDNS/static/css/main.3c2033fb.chunk.css"
+  },
+  {
+    "revision": "49ec896baf6bd58e371b",
+    "url": "/fetchDNS/static/js/2.e7b02c8b.chunk.js"
+  },
+  {
+    "revision": "4afcba7b00242bd16b89",
+    "url": "/fetchDNS/static/js/main.fa04fe91.chunk.js"
+  },
+  {
+    "revision": "a9164fce2ebb9d203af5",
+    "url": "/fetchDNS/static/js/runtime~main.feb1ae22.js"
+  }
+]);
